1f622b552cf3281288c3587ffe3d8309c85ad378
AI-Discoveries85 <161868259+AI-Discoveries85@users.noreply.github.com>
2024-03-23 02:35:14 +0500

    Add files via upload

---
+++ Q23.js
@@ -0,0 +1,16 @@
+"use strict";
+//Write a series of conditional tests. 
+//creating care variable
+let car = 'subaru';
+//Test no.1
+console.log("is car === 'subaru'? I predict true.");
+console.log(car === 'subaru');
+//Test no.2
+console.log("is car == 'subaru'? I predict true.");
+console.log(car == 'subaru');
+//Test no.3
+console.log("is car != 'Hond'? I predict true.");
+console.log(car != 'Honda');
+//Test no.4
+console.log("is car !== 'ford'? I predict true.");
+console.log(car !== 'ford');
---
+++ Q23.ts
@@ -0,0 +1,46 @@
+
+//Write a series of conditional tests. 
+
+//creating care variable
+let car :string = 'subaru';  
+
+//Test no.1
+console.log("is car === 'subaru'? I predict true.");
+console.log(car === 'subaru') ; 
+
+//Test no.2
+console.log("is car == 'subaru'? I predict true.");
+console.log(car == 'subaru');  
+
+//Test no.3
+console.log("is car != 'Hond'? I predict true.");
+console.log(car != 'Honda');  
+
+//Test no.4
+console.log("is car !== 'ford'? I predict true.");
+console.log(car !== 'ford'); 
+
+//Test no.5
+console.log("is car.Upper case == 'SUBARU'? I predict true.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+//Test no.6
+console.log("is car == 'SUBARU'? I predict False.");
+console.log(car == 'SUBARU');
+
+//Test no.7
+console.log("is car === 'SUBARU'? I predict False.");
+console.log(car === 'SUBARU');
+
+//Test no.8
+console.log("is car === 'Train'? I predict False.");
+console.log(car === 'Train'); 
+
+//Test no.9
+console.log("is car === 'Bus'? I predict False.");
+console.log(car === 'Bus'); 
+
+//Test no.10
+console.log("is car === 'Cycle'? I predict False.");
+console.log(car === 'Cycle');
+
