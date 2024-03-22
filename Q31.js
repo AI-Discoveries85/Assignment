679cbb9de4337b5cd6742c6b8558aa5854802050
AI-Discoveries85 <161868259+AI-Discoveries85@users.noreply.github.com>
2024-03-23 02:42:31 +0500

    Add files via upload

---
+++ Q31.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = ['admin', 'Eric', 'Ali', 'Aysha', 'Asma'];
+if (userName.length === 0) {
+    console.log('we need to find some users');
+}
+else {
+    userName = [];
+    console.log('All user name has been removed');
+}
---
+++ Q31.ts
@@ -0,0 +1,11 @@
+
+let userName: string[] = ['admin', 'Eric', 'Ali', 'Aysha','Asma'];
+
+if(userName.length === 0){
+
+    console.log('we need to find some users');
+}
+else{
+userName = []; 
+    console.log('All user name has been removed');
+}
