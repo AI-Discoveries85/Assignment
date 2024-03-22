bd2557c59d109b10898f99069e6add6b2e1d4b11
AI-Discoveries85 <161868259+AI-Discoveries85@users.noreply.github.com>
2024-03-23 01:55:39 +0500

    Add files via upload

---
+++ Q12.ts
@@ -0,0 +1,9 @@
+let names :string[] = ['Eric', 'Albert Einstein' , 'Ali' , 'Ejaz']; 
+
+
+const message :string = 'Welcome to typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+  console.log(message + names[i]); 
+}
