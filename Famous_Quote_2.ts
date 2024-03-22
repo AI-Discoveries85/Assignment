e79b5455ac301757d8a510f581c88bf66c0b32c8
AI-Discoveries85 <161868259+AI-Discoveries85@users.noreply.github.com>
2024-03-23 01:29:10 +0500

    Add files via upload

---
+++ Famous_Quote_2.ts
@@ -0,0 +1,5 @@
+let famous_personn :string = 'Albert Einstein';  
+
+let message :string = '“A person who never made a mistake never tried anything new.”'; 
+
+console.log(`${famous_personn} once said, ${message}`); 
