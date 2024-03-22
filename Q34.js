42054e8097efeb72765876092281eb47e50b72be
AI-Discoveries85 <161868259+AI-Discoveries85@users.noreply.github.com>
2024-03-23 02:46:39 +0500

    Add files via upload

---
+++ Q34.js
@@ -0,0 +1,6 @@
+"use strict";
+let pizzas = ['BBQ boti', 'malli boti', 'seekh kabab'];
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log(`I really love pizza!`);
---
+++ Q34.ts
@@ -0,0 +1,8 @@
+
+let pizzas: string[] = ['BBQ boti', 'malli boti', 'seekh kabab'];
+
+for(let pizza of pizzas){
+    console.log(`I like ${pizza} pizza.`);
+} 
+
+console.log(`I really love pizza!`);
